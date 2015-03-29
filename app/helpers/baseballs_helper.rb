3eb5bbfe88db84adfa54f3f847195cedbd6e7a2e
JakeCrandell <crande39@msu.edu>
2015-03-29 17:58:50 -0400

    fixing BA display

--- app/helpers/baseballs_helper.rb
+++ app/helpers/baseballs_helper.rb
@@ -1,2 +1,6 @@
 module BaseballsHelper
+  def format_batting_average(number)
+    average = number_with_precision number, precision: 3
+    number < 1 ? average[1..-1] : average
+  end
 end
--- app/models/baseball.rb
+++ app/models/baseball.rb
@@ -2,5 +2,5 @@ class Baseball < ActiveRecord::Base
   validates_presence_of :name, :position, :age, :batting_average
   validates :age, length: { is: 2, message: "MLB players must be 17 or older." }
   validates :age, numericality: { greater_than: 16, message: "MLB players must be 17 or older." }
-  validates :batting_average, length: { maximum: 5, message: "Only enter the digits of the players average, no decimals or 0. before it." }
+  validates :batting_average, length: { maximum: 5, message: "Please enter Batting Average as .XXX" }
 end
